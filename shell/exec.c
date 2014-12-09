8cae408e05f592168c49735e80252cbcccb219ed
Ryan Rose <ryanthrose@gmail.com>
2014-12-08 23:18:11 -0600

    #3 IGNORE

--- shell/exec.c
+++ shell/exec.c
@@ -8,13 +8,10 @@
 #include "exec.h"
 
 void exec(struct command *cmd) {
-    int i;
-    int input_fd;
-    int output_fd;
+    int i; int j;
+    int input_fd; int output_fd;
     int fds[2];
-    int* input_array_fd;
-    int* output_array_fd;
-    int* child_array_pid;
+    int* input_array_fd; int* output_array_fd; int* child_array_pid;
     int pid;
 
     input_fd = 0; // 0 is default for keyboard. Else is PID of file.
@@ -32,32 +29,47 @@ void exec(struct command *cmd) {
     output_fd = 1;
 
     for( i = 1; i < cmd->num_cmds; i++ ) {
-	output_fd = (cmd->cmd_args[i][2] != NULL) ? open(cmd->cmd_args[i][2], O_WRONLY) : 1;
-	input_array_fd[i] = input_fd;
-	output_array_fd[i] = output_fd;
+	// output_fd = (cmd->cmd_args[i][2] != NULL) ? open(cmd->cmd_args[i][2], O_WRONLY) : 1;
+	pipe(fds);
+	input_array_fd[i] = fds[1];
+	output_array_fd[i] = fds[0];
     }
 
     for( i = 0; i < cmd->num_cmds; i++) {
 	pid = fork();
 	if (pid == 0) {
+	    write(1,"Child Process\n", 14);
 	    close(0);
 	    dup(input_array_fd[i]);
 	    close(1);
 	    dup(output_array_fd[i]);
-	    execvp(cmd->cmd_args[i][0], cmd_args[i]);
+	    
+	    for( j = 0; j < cmd->num_cmds; j++ ) {
+		close(input_array_fd[j]);
+		close(output_array_fd[j]);
+	    }
+	   // close(input_array_fd[i]);
+	   // close(output_array_fd[i]);
+	    free(input_array_fd);
+	    free(output_array_fd);
+	    free(child_array_pid);
+
+	    execvp(cmd->cmd_args[i][0], cmd->cmd_args[i]);
 	    write(1, "Command not found\n", 19);
 	    exit(-1);
 	} else {
+	    write(1, "Parent Process\n", 15);
 	    child_array_pid[i] = pid;
         }
     }
 
-    if ( pid != 0 ) {
-	for( i = 0; i < cmd->num_cmds; i++ ) {
-	    close(input_array_fd[i]);
-	    close(output_array_fd[i]);
-	    waitpid(child_array_pid[i], NULL, 0);
-	}
+    for( i = 0; i < cmd->num_cmds; i++ ) {
+	close(input_array_fd[i]);
+	close(output_array_fd[i]);
+	waitpid(child_array_pid[i], NULL, 0);
     }
 
+    free( input_array_fd );
+    free( output_array_fd );
+    free( child_array_pid );
 }
