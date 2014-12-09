1e91a62c92a600f3972ee0bbeba33ae068dd731b
Ryan Rose <ryanthrose@gmail.com>
2014-12-09 00:29:55 -0600

    #4 IGNORE

--- shell/exec.c
+++ shell/exec.c
@@ -21,17 +21,20 @@ void exec(struct command *cmd) {
     child_array_pid = (int*) malloc(sizeof(int)*cmd->num_cmds);
 
     if (cmd->src_file != NULL) input_fd = open(cmd->src_file, O_RDONLY);
-    if (cmd->dst_file != NULL) output_fd = open(cmd->dst_file, O_WRONLY);
-
+    if (cmd->dst_file != NULL) {
+	output_fd = open(cmd->dst_file, O_WRONLY);
+    } else {
+	pipe(fds);
+	output_fd = fds[0];
+    }
+    output_array_fd[0] = output_fd;	
     input_array_fd[0] = input_fd;
-    output_array_fd[0] = output_fd;
-    input_fd = 0;
-    output_fd = 1;
+    
 
     for( i = 1; i < cmd->num_cmds; i++ ) {
 	// output_fd = (cmd->cmd_args[i][2] != NULL) ? open(cmd->cmd_args[i][2], O_WRONLY) : 1;
-	pipe(fds);
 	input_array_fd[i] = fds[1];
+	pipe(fds);
 	output_array_fd[i] = fds[0];
     }
 
@@ -44,15 +47,12 @@ void exec(struct command *cmd) {
 	    close(0);
 	    dup(output_array_fd[i]);
 	    
-//	    for( j = 0; j < cmd->num_cmds; j++ ) {
-//		close(input_array_fd[j]);
-//		close(output_array_fd[j]);
-//	    }
-	   // close(input_array_fd[i]);
-	   // close(output_array_fd[i]);
-//	    free(input_array_fd);
-//	    free(output_array_fd);
-//	    free(child_array_pid);
+	    for( j = 1; j < cmd->num_cmds; j++ ) {
+		close(input_array_fd[j]);
+		close(output_array_fd[j]);
+	    }
+//	   close(input_array_fd[i]);
+//	   close(output_array_fd[i]);
 //	    write(1, cmd->cmd_args[i][0], 5);
 //	    write(1, cmd->cmd_args[i][1], 5);
 	    execvp(cmd->cmd_args[i][0], cmd->cmd_args[i]);
@@ -65,11 +65,19 @@ void exec(struct command *cmd) {
     }
     write(1, "DO IT\n", 7);
     for( i = 0; i < cmd->num_cmds; i++ ) {
-//	waitpid(child_array_pid[i], &child_array_pid[i], 0);
+	waitpid(child_array_pid[i], NULL, 0);
 //	close(input_array_fd[i]);
 //	close(output_array_fd[i]);
-	waitpid(child_array_pid[i], NULL, 0);
+//	waitpid(child_array_pid[i], NULL, 0);
     }
+
+//    for( i = 0; i < cmd->num_cmds; i++ ) {
+//	close(3);
+//	dup(input_array_fd[i]);
+//	close(input_array_fd[i]);
+//	dup(output_array_fd[i]);
+//	close(output_array_fd[i]);
+//    }
 
     free( input_array_fd );
     free( output_array_fd );
